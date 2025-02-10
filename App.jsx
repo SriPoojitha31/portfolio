69bf6e6c6ff52a67028469a31e84e4a2cef5a256
JORIGE SRI POOJITHA <164748077+SriPoojitha31@users.noreply.github.com>
2025-02-10 19:21:05 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+function App() {
+    return(
+        <div className="container">
+            <header>
+                <h1>Sri Poojitha Jorige</h1>
+                <p>Specialisation in AIML | Web developer</p>
+            </header>
+            <section>
+                <h2>About Me</h2>
+                <p>I am a passionate deeveloper with expertise in React, Javascript, C, C++, Python, Java and other web technologies.
+                I love building scalable and efficient applications, I have a good communication skills and many other qualities also.</p>
+            </section>
+
+            <section>
+                <h2>Projects</h2>
+                <ul>
+                    <li>
+                        <strong>Portfolio Website</strong>-A personal website showcasing my work.
+                    </li>
+                    <li>
+                        <strong>To-do app</strong>- A web page to manage to do tasks.
+                    </li>
+                    <li>
+                        <strong>Signup-Login form</strong>- A web page of signup and login form.
+                    </li>
+                    <li>
+                        <strong>Fault detection in transmitter using UNO board</strong>- A fault detection of fire and power transmission using uno Board.
+                    </li>
+                    <li>
+                        <strong>ExploreHive</strong>- A webpage that helps tourists.
+                    </li>
+                    <li>
+                        <strong>XenoPitch</strong>- A webpage that helps Gamers.
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <h2>Contacts</h2>
+                <p>Email:poojitha.23bce20024@vitapstudent.ac.in</p>
+                <p>Email: sripoojitha.2006@gmail.com</p>
+                <p>LinkedIn: www.linkedin.com/in/sri-poojitha-jorige-377270294</p>
+            </section>
+        </div>
+    );
+}
+
+export default App;
